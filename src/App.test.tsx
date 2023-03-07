b01305f34d6fa21c325a88f3fe73a639fd50c109
Bryan KANEB <bryankaneb2@gmail.com>
2023-03-07 23:18:50 +0100

    test(App): remove test on the background color

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,12 +5,6 @@ import App from "./App";
 afterEach(cleanup);
 
 describe("App", () => {
-  it("background color is #333", () => {
-    render(<App />);
-    const app = screen.getByTestId("app");
-    expect(app).toHaveStyle("background: #333");
-  });
-
   it("the children is centered", () => {
     render(<App />);
     const app = screen.getByTestId("app");
